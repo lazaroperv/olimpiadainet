1831947e53c95537efc009a2cb734121d03c60b7
lazaroperv <lazaroperv18@gmail.com>
2024-08-15 17:10:10 -0300

    logg

--- CapaCliente/Views/Pages/login.php
+++ CapaCliente/Views/Pages/login.php
@@ -1,9 +1,11 @@
+
 <section>
 
     <form action="" method="post">
-
-        
-
+            <input type="text" placeholder="ingrese su Nombre" required>
+            <input type="email" name="" id="" placeholder="ingrese su email" required>
+            <input type="password" name="" id="" placeholder="ingrese contraseña"required>
+            <input type="submit" name="inicio">
     </form>
 
 </section>
