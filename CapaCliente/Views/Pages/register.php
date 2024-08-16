f255d14740a87dec0c064fac52c45119b5d8f9c8
lazaroperv <lazaroperv18@gmail.com>
2024-08-16 01:51:48 -0300

    avances !!

--- CapaCliente/Views/Pages/register.php
+++ CapaCliente/Views/Pages/register.php
@@ -28,12 +28,13 @@
         <main>
 
 
+            <section class="register-users">
+                <form action="" method="post">
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                 Repudiandae repellat consectetur amet doloribus eligendi ex nam officiis 
-                 sequi reprehenderit in? Eligendi dignissimos 
-                inventore vero exercitationem ipsum fuga in eum provident?</p>
+                    
 
+                </form>
+            </section>
 
         </main>
 
