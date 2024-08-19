2afd952ccdcb2e9b7f9b74ebfcf924194e80d1ca
lazaroperv <lazaroperv18@gmail.com>
2024-08-19 14:13:14 -0300

    listd

--- CapaCliente/Views/Pages/login.php
+++ CapaCliente/Views/Pages/login.php
@@ -6,11 +6,20 @@
 <section>
 
     <div class="iniciar sesion">
+<<<<<<< HEAD
     <form action="" method="post">
             <input type="email" name="email" id="" placeholder="ingrese su email" required>
             <input type="password" name="contraseña" id="" placeholder="ingrese contraseña"required>
             <input type="submit" name="Ingresar" value="ingresar">
     </form>
     <a href="RegistroU.php">Registrate!</a>
+=======
+        <form action="" method="post">
+                <input type="email" name="email" id="" placeholder="ingrese su email" required>
+                <input type="password" name="contraseña" id="" placeholder="ingrese contraseña"required>
+                <input type="submit" name="Ingresar" value="Ingresar">
+        </form>
+        <a href="registro.php">Registrate!!</a>
+>>>>>>> 4f89992 (listd)
     </div>
 </section>
--- CapaServidor/Model/Models/modific.php
+++ CapaServidor/Model/Models/modific.php
@@ -1,6 +1,15 @@
 <?php
+<<<<<<< HEAD
+<<<<<<<< HEAD:CapaServidor/Model/Models/modific.php
 
     if(!empty($_POST["ModificarP"])){
+========
+    include '../../Controller/conexion.php';
+?>
+<?php
+   
+    if(!empty($_POST["SubirP"])){
+>>>>>>>> 4f89992 (listd):CapaServidor/Model/Models/alta.php
         if(empty($_POST["codigoP"]) or empty($_POST["descripcionP"]) or empty($_POST["precioP"]) or empty($_POST["stockP"]) or empty($_POST["tipoP"]) or empty($_POST["genero"])) { 
           
             echo "Debe Rellenar el Campo para Continuar!";
@@ -11,8 +20,13 @@
             $preP=$_POST["precioP"];
             $stoP=$_POST["stockP"];
             $tipP=$_POST["tipoP"];
+<<<<<<<< HEAD:CapaServidor/Model/Models/modific.php
             $genP=$_POST["genero"];
             $sql=$ruta->query("update productos set descripcion='$desP', precio_unitario='$preP', stock='$stoP',tipo='$tipP',genero='$genP' where codigo_producto='$codP'");
+========
+            $gen=$_POST["genero"];
+            $sql=$ruta->query("insert into Productos (codigo_producto,descripcion,precio_unitario,stock,tipo,genero	) values ('$codP','$desP','$preP','$stoP','$tipP','$gen')");
+>>>>>>>> 4f89992 (listd):CapaServidor/Model/Models/alta.php
             if($sql==1){
                 echo "Producto Actualizado!";
             }
@@ -20,5 +34,39 @@
                 echo "No se ha Modificado";    
             } 
         }       
+=======
+    if(!empty($_POST["modificar"])){
+        if(empty($_POST["codigo"]) or empty($_POST["nombre"]) or empty($_POST["descripcion"]) or empty($_POST["precio"])) { 
+          
+            echo '<script>
+            Swal.fire({
+                title: "Incompleto!",
+                text: "Debe Rellenar el Campo para Continuar!",
+                icon: "info"
+              });
+            </script>';
+        }
+        else{
+            $cod=$_POST["codigo"];
+            $nom=$_POST["nombre"];
+            $des=$_POST["descripcion"];
+            $pre=$_POST["precio"];
+            $sql=$ruta->query("update productos set nombre='$nom', descripcion='$des', precio='$pre' where cod_pro='$cod'");
+            if($sql==1){
+                echo '<script>
+                    Swal.fire({
+                        title: "Producto Actualizado!",
+                        text: "Se Guardo en la Tabla Productos!",
+                        icon: "success"
+                    });
+                </script>';
+            }
+            else{
+                echo "No se ha Modificado";
+            }
+        }
+        
+        
+>>>>>>> 4f89992 (listd)
     }
 ?>
