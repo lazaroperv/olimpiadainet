2f4bed8ff3377ed77594dd3b42b22b2329813958
agusberal <laumaldo006@gmail.com>
2024-08-20 00:06:20 -0300

    creo

---
+++ CapaServidor/Components/estructura/main.php
@@ -0,0 +1,13 @@
+<main>
+
+
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Repudiandae repellat consectetur amet doloribus eligendi ex nam officiis
+            sequi reprehenderit in? Eligendi dignissimos
+            inventore vero exercitationem ipsum fuga in eum provident?
+        </p>
+
+
+
+</main>
